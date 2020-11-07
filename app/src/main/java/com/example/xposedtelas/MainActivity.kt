36ec2c68b422a81285ebe6a413fc411dd40e83bc
Vinícius Monteiro <47702719+Viniiciusmonterio@users.noreply.github.com>
2020-11-06 21:52:43 -0300

    Atualização da validação de login

--- app/src/main/java/com/example/xposedtelas/MainActivity.kt
+++ app/src/main/java/com/example/xposedtelas/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.xposedtelas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.Toast
 import com.example.Model.Client
@@ -23,30 +24,50 @@ class MainActivity : AppCompatActivity() {
         }
 
         buttonLogar.setOnClickListener{
-            this.check()
+            this.validarCampos()
         }
     }
 
-    public fun check(){
+    private fun check() {
         val email: String = editTextTextEmailAddress.text.toString();
         val senha: String = editTextTextPassword.text.toString();
-        val nome: String = ""
-        val telefone : String = ""
-        val cpf : String = ""
-        val client = Client(nome = nome,telefone = telefone, email = email, senha = senha, cpf = cpf)
+        val nome: String = " ";
+        val telefone: String = " ";
+        val cpf: String = " ";
+        val client = Client(nome = nome, telefone = telefone, email = email, senha = senha, cpf = cpf);
 
         ClientWeb().select(client, {
 
-            if (email.equals("")  && senha.equals("")){
-                Toast.makeText(this, "Por favor informe seu email e senha", Toast.LENGTH_LONG).show()
-            }
+            Toast.makeText(this, "Entrou", Toast.LENGTH_LONG).show()
+            val intent = Intent(this,activity_rank ::class.java)
+            startActivity(intent)
 
-            if(email.equals(it.email) && senha.equals(it.senha)){
-                val intent = Intent(this, activity_rank::class.java)
-                startActivity(intent)
-            }
         }, {
-            Toast.makeText(this, "Usuário não encontrado", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Email ou Senha incorreto", Toast.LENGTH_LONG).show()
         })
+
     }
+
+    private fun validarCampos() {
+        val email: String = editTextTextEmailAddress.text.toString();
+        val senha: String = editTextTextPassword.text.toString();
+
+        if (TextUtils.isEmpty(email)) {
+            editTextTextEmailAddress.error = "Preencha o campo de Email";
+        } else if (email.trim().length < 10) {
+            editTextTextEmailAddress.error = "O Email não pode ter menos de 10 Caracteres";
+        };
+
+        if (TextUtils.isEmpty(senha)) {
+            editTextTextPassword.error = "Preencha o campo de Senha";
+        } else if (senha.trim().length < 8) {
+            editTextTextPassword.error = "A Senha não pode ter menos de 8 Letras ";
+        };
+        else {
+            this.check();
+        }
+
+
+    }
+
 }
